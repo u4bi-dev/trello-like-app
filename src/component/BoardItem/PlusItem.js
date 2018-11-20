a60906fb63d8085545ab911e6acb33dc79c2fb9c
myungjaeyu <rootcode10@gmail.com>
2018-11-21 01:32:59 +0900

    Added boardItem/PlusItem component.

---
+++ src/component/BoardItem/PlusItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './PlusItem.css'
+
+const BoardPlusItem = () => {
+
+    return (
+    <div className='BoardPlusItem'>
+        +
+    </div>
+    )
+
+}
+
+export default BoardPlusItem
