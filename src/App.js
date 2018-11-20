6006ba80c11f9a70cbf1f99e4453ac7f14f11b1b
myungjaeyu <rootcode10@gmail.com>
2018-11-21 01:36:10 +0900

    Added board to app component.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import './App.css'
 import { Subscribe } from 'unstated'
 import AppContainer from './provider/container/AppContainer'
 
+import Board from './component/Board'
+
 class App extends Component {
     render() {
         return (
         <Subscribe to={[AppContainer]}>
             { (app) => (
             <div className='App'>
+
+                <Board />
+
+                <Board />
+
+                <Board />
+
+                <Board />                
 
             </div>) }
         </Subscribe>
