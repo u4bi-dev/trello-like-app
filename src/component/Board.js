076b89f41a54944bf9a5a1a1a0b2c905d424d7eb
myungjaeyu <rootcode10@gmail.com>
2018-11-21 01:34:38 +0900

    Added overflow scrolling and board title to board component.

--- src/component/Board.js
+++ src/component/Board.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 import './Board.css'
 
+import BoardItem from './BoardItem'
+
 class Board extends Component {
     render() {
         return (
         <div className='Board'>
+
+            <div className='Board__Title'>
+                Title
+            </div>
+
+            <div className='Board__Item_Wrapper'>
+
+                <BoardItem isPlus={ false } />
+
+                <BoardItem isPlus={ false } />
+
+                <BoardItem isPlus={ false } />
+
+                <BoardItem isPlus={ false } />
+
+                <BoardItem isPlus={ true } />
+
+            </div>
 
         </div>)
     }
