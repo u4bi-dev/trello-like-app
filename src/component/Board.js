53f4236ce46910b6039d34ed26848db4b92993cf
myungjaeyu <rootcode10@gmail.com>
2018-11-21 00:22:49 +0900

    Added board component.

---
+++ src/component/Board.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './Board.css'
+
+class Board extends Component {
+    render() {
+        return (
+        <div className='Board'>
+
+        </div>)
+    }
+}
+
+export default Board
